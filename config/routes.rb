00d1e94fed57475bb9f6a5fc1ea20fee5b4a7d26
Mateo Campos <campos129823@gmail.com>
2024-09-07 00:58:54 -0500

    Create, Update, POST

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get '/health', to: 'health#health'
 
-  resources :posts, only: [:index, :show]
+  resources :posts, only: [:index, :show, :create, :update]
 
   # Defines the root path route ("/")
   # root "posts#index"
