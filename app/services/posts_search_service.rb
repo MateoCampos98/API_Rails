06990ee680ceee802de907f74a928ad537b9f786
Mateo Campos <campos129823@gmail.com>
2024-09-09 12:51:44 -0500

    Caching cast cache

--- app/services/posts_search_service.rb
+++ app/services/posts_search_service.rb
@@ -1,5 +1,10 @@
 class PostsSearchService 
   def self.search(curr_post, query)
-    curr_post.where("title LIKE ?", "%#{query}%")
+    # {k => v}
+    posts_ids = Rails.cache.fetch("posts_search/#{query}", expires_in: 1.hours) do
+    curr_post.where("title LIKE ?", "%#{query}%").map(&:id)
+    end
+
+    curr_post.where(id: posts_ids)
   end
 end
